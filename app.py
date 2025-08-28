f91a4c276886ae55265a3eb787ec7eda3d2764ec
Bastaocchi <gui.vino@gmail.com>
2025-08-28 07:51:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
     page_title="Gerenciador de Símbolos",
     page_icon="📊",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="collapsed"  # Sidebar minimizada
 )
 
 # CSS personalizado para interface moderna
