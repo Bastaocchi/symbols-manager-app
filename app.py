06e22b29030afb0bcf49e85ef10903c38d9fe1ae
Bastaocchi <gui.vino@gmail.com>
2025-08-28 14:36:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -50,16 +50,16 @@ td {
 tr:nth-child(odd) { background-color: #15191f !important; }
 tr:nth-child(even) { background-color: #1b1f24 !important; }
 th:nth-child(1), td:nth-child(1) { width: 100px !important; }
-th:nth-child(2) { width: 300px !important; text-align: center !important; }
+th:nth-child(2) { width: 400px !important; text-align: center !important; }
 td:nth-child(2) {
-    width: 300px !important;
+    width: 400px !important;
     white-space: nowrap !important;
     overflow: hidden !important;
     text-overflow: ellipsis !important;
     text-align: left !important;
 }
 th:nth-child(3), td:nth-child(3) { width: 220px !important; }
-th:nth-child(4), td:nth-child(4) { width: 200px !important; }
+th:nth-child(4), td:nth-child(4) { width: 280px !important; }
 th:nth-child(5), td:nth-child(5) {
     width: 220px !important;
     font-size: 22px !important;
